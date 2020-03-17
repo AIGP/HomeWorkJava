c97b5218696217a210b09ac6bef862efbc98ee0e
AIGP <aigp@yandex.ru>
2020-03-17 10:52:10 +0300

    Домашняя работа по Java #2.

---
+++ src/Java2_Home_Work4/ClientGUI.java
@@ -0,0 +1,131 @@
+package Java2_Home_Work4;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ClientGUI extends JFrame implements ActionListener, KeyListener, Thread.UncaughtExceptionHandler {
+
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 300;
+
+    private final JTextArea log = new JTextArea();
+    private final JPanel panelTop = new JPanel(new GridLayout(2, 3));
+    private final JTextField tfIPAddress = new JTextField("127.0.0.1");
+    private final JTextField tfPort = new JTextField("8189");
+    private final JCheckBox cbAlwaysOnTop = new JCheckBox("Always on top");
+    private final JTextField tfLogin = new JTextField("ivan");
+    private final JPasswordField tfPassword = new JPasswordField("123");
+    private final JButton btnLogin = new JButton("Login");
+
+    private final JPanel panelBottom = new JPanel(new BorderLayout());
+    private final JButton btnDisconnect = new JButton("<html><b>Disconnect</b></html>");
+    private final JTextField tfMessage = new JTextField();
+    private final JButton btnSend = new JButton("Send");
+
+    private final JList<String> userList = new JList<>();
+
+    public static void main(String[] args) {
+        //Записываем в файл сообщение: "Начало работы".
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ClientGUI();
+            }
+        });
+    }
+
+    private ClientGUI() {
+        Thread.setDefaultUncaughtExceptionHandler(this);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent event) {
+                //В логирование записываем "Конец работы" и закрываем файл
+            }
+        });
+        setLocationRelativeTo(null);
+        setSize(WIDTH, HEIGHT);
+        setTitle("Chat client");
+        String[] users = {"user1", "user2", "user3", "user4", "user5",
+                "user_with_an_exceptionally_long_name_in_this_chat"};
+        userList.setListData(users);
+        log.setEditable(false);
+        JScrollPane scrollLog = new JScrollPane(log);
+        JScrollPane scrollUsers = new JScrollPane(userList);
+        scrollUsers.setPreferredSize(new Dimension(100, 0));
+        cbAlwaysOnTop.addActionListener(this);
+        btnSend.addActionListener(this);
+        tfMessage.addKeyListener(this);
+
+        panelTop.add(tfIPAddress);
+        panelTop.add(tfPort);
+        panelTop.add(cbAlwaysOnTop);
+        panelTop.add(tfLogin);
+        panelTop.add(tfPassword);
+        panelTop.add(btnLogin);
+        panelBottom.add(btnDisconnect, BorderLayout.WEST);
+        panelBottom.add(tfMessage, BorderLayout.CENTER);
+        panelBottom.add(btnSend, BorderLayout.EAST);
+
+        add(scrollLog, BorderLayout.CENTER);
+        add(scrollUsers, BorderLayout.EAST);
+        add(panelTop, BorderLayout.NORTH);
+        add(panelBottom, BorderLayout.SOUTH);
+
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object src = e.getSource();
+        if (src == cbAlwaysOnTop) {
+            setAlwaysOnTop(cbAlwaysOnTop.isSelected());
+            //Always on top теперь cbAlwaysOnTop.isSelected()
+        }
+        else if (src == btnSend) {
+            if(!tfMessage.getText().toString().isEmpty()) {
+                log.append(tfMessage.getText() + "\n");
+                //Отправлено такое-то сообщение
+            }
+            tfMessage.setText("");
+        }
+        else
+            throw new RuntimeException("Unknown source: " + src);
+    }
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        e.printStackTrace();
+        String msg;
+        StackTraceElement[] ste = e.getStackTrace();
+        msg = "Exception in thread " + t.getName() + " " +
+                e.getClass().getCanonicalName() + ": " +
+                e.getMessage() + "\n\t" + ste[0];
+        //Записываем в лог
+        JOptionPane.showMessageDialog(null, msg, "Exception", JOptionPane.ERROR_MESSAGE);
+        System.exit(1);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            if(!tfMessage.getText().toString().isEmpty()) {
+                log.append(tfMessage.getText() + "\n");
+                //Отправлено такое-то сообщение
+            }
+            tfMessage.setText("");
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+
+    }
+
+}
+
